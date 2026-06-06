6e285816c8f48d14def57fb70a24fb45bb370864
Rico Apon <rico.apon@gmail.com>
2026-06-06 10:16:35 +0200

    Active ScrollSpy to activate the navlink that corresponds to the page

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,4 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
+    // Activate Bootstrap scrollspy on the main nav element.
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: mainNav.offsetHeight,
+        });
+    }
+
     // When on mobile variant, clicking a nav link doesn't automatically collapse the navbar. This code ensures it collapses.
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
